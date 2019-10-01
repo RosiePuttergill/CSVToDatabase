25db73127840971ab58a4adfe9bcaf2a44750c91
Rosie Puttergill <rosieputtergill@hotmail.co.uk>
2019-10-01 18:55:53 +0100

    CSVToDatabase
    My ASP.NET program to upload a CSV file following a certain schema to persist data to a database and show the contents of the table in a webpage.
    
    Schema:
    Name: NVARCHAR(50), NON NULL
    DOB: Date, NON NULL
    CreditLimit: DECIMAL, 2DP, NON NULL
    YearsAtCurrentAddress: INT32, NON NULL
    PolicyId: GUID, NON NULL
    
    Included 4 example CSV files, 3 valid and 1 incompatible.

---
+++ CsvToDatabase/CsvToDatabase/Pages/Index.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CsvToDatabase.Pages.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CSV to Database</title>
+    <style type="text/css">
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>
+                Upload a CSV file matching the following schema to upload data to the database for display:
+            </h3>
+            <p>
+                Name: NVARCHAR(50), NON NULL<br />
+                DOB: Date, NON NULL<br />
+                CreditLimit: DECIMAL, 2DP, NON NULL<br />
+                YearsAtCurrentAddress: INT32, NON NULL<br />
+                PolicyId: GUID, NON NULL
+            </p>
+            <hr />
+            <asp:FileUpload ID="FileUpload" runat="server" />
+            <asp:Button ID="UploadButton" Text="Upload" runat="server"  OnClick="UploadButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Info" ForeColor="Black" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="ErrorTitle" ForeColor="Black" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="ErrorMessage" ForeColor="Black" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="ClearButton" Text="Clear Table" runat="server" OnClick="ClearButton_Click"/>
+            <asp:GridView ID="DataGrid" runat="server" AutoGenerateColumns="false" CellPadding="2">
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name"/>
+                    <asp:BoundField DataFormatString="{0:dd MMM yyyy}" DataField="DOB" HeaderText="Date of Birth"  />
+                    <asp:BoundField DataFormatString="{0:C2}" DataField="CreditLimit" HeaderText="Credit Limit" />
+                    <asp:BoundField DataField="YearsAtCurrentAddress" HeaderText="Years at Current Address" />
+                    <asp:BoundField DataField="PolicyId" HeaderText="Policy ID" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CsvToDatabase/CsvToDatabase/Pages/Index.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using System.Data.Entity.Core.EntityClient;
+
+namespace CsvToDatabase.Pages
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PopulateDataGrid();
+        }
+
+        private void PopulateDataGrid()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    SqlDataAdapter cmd = new SqlDataAdapter("SELECT * FROM [Clients]", con);
+                    con.Open();
+                    DataTable dtbl = new DataTable();
+                    cmd.Fill(dtbl);
+                    DataGrid.DataSource = dtbl;
+                    DataGrid.DataBind();
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+
+        }
+        private string GetConnectionString()
+        {
+            EntityConnectionStringBuilder builder = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStrings["DatabaseEntities"].ConnectionString);
+            return builder.ProviderConnectionString;
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            ProcessFile();
+            PopulateDataGrid();
+        }
+
+        private void ProcessFile()
+        {
+            if (FileUpload.HasFile)
+            {
+                FileInfo fileInfo = new FileInfo(FileUpload.PostedFile.FileName);
+                if (fileInfo.Name.EndsWith(".csv"))
+                {
+                    string fileName = fileInfo.Name.Replace(".csv", "").ToString();
+                    string csvFilePath = Server.MapPath("CSV") + "\\" + fileInfo.Name;
+
+                    FileUpload.SaveAs(csvFilePath);
+
+                    try
+                    {
+                        int rowsAdded = UploadDataToDatabase("Clients", csvFilePath, ",");
+                        Info.Text = string.Format("({0}) records have been uploaded", rowsAdded);
+                        ErrorTitle.Text = "";
+                        ErrorMessage.Text = "";
+                    }
+                    catch (SqlException ex)
+                    {
+                        Info.Text = "";
+                        ErrorTitle.Text = "CSV contents not uploaded. See error below:";
+                        ErrorMessage.Text = ex.Message;
+                    }
+                }
+                else
+                {
+                    ErrorTitle.Text = "Unable to recognise file";
+                }
+            }
+        }
+
+        private int UploadDataToDatabase(string tableName, string filePath, string delimeter)
+        {
+            ClearTable("StagingClients");
+            BulkInsertToStagingTable(filePath, delimeter);
+            return InsertIntoClientTable();
+        }
+
+        private void ClearTable(string tableName)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat(string.Format("DELETE FROM {0}", tableName));
+            ExecuteNonQuery(stringBuilder.ToString());
+            ErrorTitle.Text = "";
+            ErrorMessage.Text = "";
+        }
+
+        private int ExecuteNonQuery(string sqlQuery)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(GetConnectionString()))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection))
+                {
+                    int rows = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                    return rows;
+                }
+            }
+        }
+
+        private void BulkInsertToStagingTable(string filePath, string delimeter)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("BULK INSERT StagingClients");
+            stringBuilder.AppendFormat(string.Format(" FROM '{0}'", filePath));
+            stringBuilder.AppendFormat(string.Format(" WITH (FIELDTERMINATOR = '{0}' , ROWTERMINATOR = '\n' )", delimeter));
+            ExecuteNonQuery(stringBuilder.ToString());
+        }
+
+        private int InsertIntoClientTable()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("INSERT INTO Clients (Name, DOB, CreditLimit, YearsAtCurrentAddress, PolicyId) ");
+            stringBuilder.Append("SELECT Name, DOB, CreditLimit, YearsAtCurrentAddress, PolicyId FROM StagingClients");
+            return ExecuteNonQuery(stringBuilder.ToString());
+        }
+
+        protected void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearTable("Clients");
+            PopulateDataGrid();
+        }
+    }
+}
